633d5eb70f27ecb1249baef2ff0bd04c19c5f860
RePsycho <e.v.haaften@live.nl>
2019-10-26 15:10:55 +0200

    To save it all

--- src/Controller/controller.php
+++ src/Controller/controller.php
@@ -23,21 +23,23 @@ class controller extends AbstractController
         return $this->render('form.html.twig', ['name' => $name]);
 
         //opslag
-        $formulier = new Formulier();
+        $formulier =new Formulier();
 
         $formulier->setName('Ewout');
         $formulier->setEmail('vleermuis@live');
-        $formulier->getMessage();
+        $formulier->getMessage(text'Hallo ik heet ewout');
         $formulier->getTimeofpost();
 
         $em = $this->getDoctrine()->getManager();
 
-        $retrieveform = $em->getRepository(FormulierRepository::class);
+        $retrieveform = $em->getRepository(FormulierRepository::class)->findAll();
+
+        var_dump($retrieveform);
 
         $em->persist($formulier);
 
-
-
         $em->flush();
+
+        return $this->render('form.html.twig', ['name' => $name]);
     }
 }
